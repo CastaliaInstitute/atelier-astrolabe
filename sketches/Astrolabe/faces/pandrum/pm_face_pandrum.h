6260e070a7f61338818c04c33fade6e8c29ce8bc
dcmcshan <dan@syzygyx.com>
2026-05-20 17:40:34 -0600

    Use IMU impact for pandrum taps

--- sketches/Astrolabe/faces/pandrum/pm_face_pandrum.h
+++ sketches/Astrolabe/faces/pandrum/pm_face_pandrum.h
@@ -5,7 +5,9 @@
 void pm_face_pandrum_draw(void);
 void pm_face_pandrum_stop(void);
 bool pm_face_pandrum_play_at(int16_t x, int16_t y);
+bool pm_face_pandrum_motion_tick(uint32_t now_ms);
 bool pm_face_pandrum_anim_tick(uint32_t now_ms);
 const char *pm_face_pandrum_note_label(void);
 int pm_face_pandrum_note_index(void);
 float pm_face_pandrum_last_hz(void);
+float pm_face_pandrum_last_force(void);
