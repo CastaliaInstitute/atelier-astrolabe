93ebd3f15c3a3d534bddecb0e45edbd3c1df8fbb
dcmcshan <dan@syzygyx.com>
2026-05-26 20:37:55 -0600

    Add serial TTS command

--- sketches/Astrolabe/pm_voice.cpp
+++ sketches/Astrolabe/pm_voice.cpp
@@ -23,7 +23,7 @@
 
 static const char *TAG = "pm_voice";
 
-static constexpr uint32_t kVoiceNetTaskStack = 20480;
+static constexpr uint32_t kVoiceNetTaskStack = 12288;
 /** STT + Gemini + TTS + large chunked JSON (astrology readings). */
 static constexpr uint32_t kVoiceHttpTimeoutMs = 660000;
 /** voice-pipeline JSON + base64 MP3. */
