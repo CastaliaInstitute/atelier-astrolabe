5fd20683a4574b683b37af217ed773ec92c4d473
dcmcshan <dan@syzygyx.com>
2026-07-14 04:21:10 -0600

    feat: add psychometer face with affect controls

---
+++ astrolabe175c/main/faculty175_face_psych_state.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+void faculty175_face_psych_state_draw(uint32_t anim_ms);
+bool faculty175_face_psych_state_action(uint32_t seed_ms);
+bool faculty175_face_psych_state_tap(int16_t x, int16_t y);
+bool faculty175_face_psych_state_style_delta(int delta);
